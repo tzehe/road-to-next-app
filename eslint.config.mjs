7bb36fd7396fb8e9b1955d05c192b0dc48a61f65
Thomas Zehe <thzhdv@gmail.com>
2025-11-03 19:33:12 +0100

    Add eslint-plugin-simple-import-sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,26 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
